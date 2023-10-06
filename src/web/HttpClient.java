5ede5b97dafb0d8b76ae3ce6c55e2a17c03ac1aa
dsail <dsail@yandex.ru>
2023-10-06 16:47:11 +0300

    commit

---
+++ src/web/HttpClient.java
@@ -0,0 +1,8 @@
+package hw4.src.web;
+
+public class HttpClient {
+    public String get(String url) {
+        // HTTP-запрос.
+        return url;
+    }
+}
---
+++ src/web/WebService.java
@@ -0,0 +1,11 @@
+package hw4.src.web;
+
+public class WebService {
+    public HttpClient httpClient;
+    public WebService(HttpClient httpClient) {
+        this.httpClient = httpClient;
+    }
+    public String getData(String url) {
+        return httpClient.get(url);
+    }
+}
